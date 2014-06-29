4930530b76d105a6fe67dc718cc81e192c83ba05
Junya Hayashi <ledmonster@gmail.com>
2014-06-29 19:26:12 +0900

    Added FacebookSDK, Facebook configuration and WWTopViewController

---
+++ werewolf/WWTopViewController.h
@@ -0,0 +1,14 @@
+//
+//  WWTopViewController.h
+//  werewolf
+//
+//  Created by Junya Hayashi on 2014/06/29.
+//  Copyright (c) 2014年 Junya Hayashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <FacebookSDK/FacebookSDK.h>
+
+@interface WWTopViewController : UIViewController
+
+@end
